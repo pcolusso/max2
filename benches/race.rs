380931a3024553545e2b1a2d72e99ca543fa3145
Paul Colusso <paulcolusso@gmail.com>
2022-05-11 17:17:47 +1000

    more experiments!

--- benches/race.rs
+++ benches/race.rs
@@ -1,9 +1,41 @@
 use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
 use rand::Rng;
-use max2::{sort_max2, fold_max2};
+use max2::{sort_max2, fold_max2, loop_max2, transform_max2, par_max2};
 
-fn bench_max2(c: &mut Criterion) {
-    let mut group = c.benchmark_group("max2");
+fn bench_floats(c: &mut Criterion) {
+    let mut group = c.benchmark_group("floats");
+    let mut rng = rand::thread_rng();
+
+    let samples: Vec<Vec<f64>> = vec![
+        (0..10).map(|_| rng.gen_range(0.0, 10.0)).collect(),
+        (0..100).map(|_| rng.gen_range(0.0, 100.0)).collect(),
+        (0..1000).map(|_| rng.gen_range(0.0, 1000.0)).collect(),
+        (0..10000).map(|_| rng.gen_range(0.0, 10000.0)).collect(),
+    ];
+
+    for sample in samples {
+        group.bench_with_input(BenchmarkId::new("max2-sort", sample.len()), &sample, |b, sample| {
+            b.iter(|| sort_max2(sample));
+        });
+        group.bench_with_input(BenchmarkId::new("max2-fold", sample.len()), &sample, |b, sample| {
+            b.iter(|| fold_max2(sample) );
+        });
+        group.bench_with_input(BenchmarkId::new("loop-max2", sample.len()), &sample, |b, sample| {
+            b.iter(|| loop_max2(sample) );
+        });
+        group.bench_with_input(BenchmarkId::new("xform-max2", sample.len()), &sample, |b, sample| {
+            b.iter(|| transform_max2(sample) );
+        });
+        group.bench_with_input(BenchmarkId::new("par-max2", sample.len()), &sample, |b, sample| {
+            b.iter(|| par_max2(sample) );
+        });
+    }
+
+    group.finish();
+}
+
+fn bench_ints(c: &mut Criterion) {
+    let mut group = c.benchmark_group("ints");
     let mut rng = rand::thread_rng();
 
     let samples: Vec<Vec<u64>> = vec![
@@ -20,10 +52,33 @@ fn bench_max2(c: &mut Criterion) {
         group.bench_with_input(BenchmarkId::new("max2-fold", sample.len()), &sample, |b, sample| {
             b.iter(|| fold_max2(sample) );
         });
+        group.bench_with_input(BenchmarkId::new("loop-max2", sample.len()), &sample, |b, sample| {
+            b.iter(|| loop_max2(sample) );
+        });
+        group.bench_with_input(BenchmarkId::new("xform-max2", sample.len()), &sample, |b, sample| {
+            b.iter(|| transform_max2(sample) );
+        });
+        group.bench_with_input(BenchmarkId::new("par-max2", sample.len()), &sample, |b, sample| {
+            b.iter(|| par_max2(sample) );
+        });
     }
 
     group.finish();
 }
 
-criterion_group!(benches, bench_max2);
+fn bench_huge(c: &mut Criterion) {
+    let mut group = c.benchmark_group("yuge");
+    let mut rng = rand::thread_rng();
+
+    let sample: Vec<f32> = (0..10_000_000u64).map(|_| rng.gen_range(f32::MIN, f32::MAX)).collect();
+
+    group.bench_with_input(BenchmarkId::new("xform-max2", sample.len()), &sample, |b, sample| {
+        b.iter(|| transform_max2(sample) );
+    });
+    group.bench_with_input(BenchmarkId::new("par-max2", sample.len()), &sample, |b, sample| {
+        b.iter(|| par_max2(sample) );
+    });
+}
+
+criterion_group!(benches, bench_ints, bench_floats, bench_huge);
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,42 @@
-#[derive(Debug, PartialEq)]
+use std::cmp::Ordering::Equal;
+
+// Trick from https://www.worthe-it.co.za/blog/2017-01-15-aliasing-traits-in-rust.html to alias traits
+// Copy + Clone slows the sort algo, which is well, to be expected.
+pub trait Numeric: PartialOrd + Copy + Clone {}
+
+impl<T> Numeric for T
+    where T: PartialOrd + Copy + Clone {}
+
+#[derive(Debug, PartialEq, Copy, Clone)]
 pub struct Pair<T>(T, T)
 where
-    T: Ord;
+    T: Numeric;
+
+#[derive(Clone)]
+enum PairOption<T: Numeric> {
+    Empty,
+    One(T),
+    Pair(Pair<T>)
+}
 
 // Clones iter into a vec, sorts it, and returns the top 2. Very naive impl
 pub fn sort_max2<I, T>(input: I) -> Pair<T>
 where
     I: IntoIterator<Item = T>,
-    T: Ord,
+    T: Numeric,
 {
     let mut sorted = input.into_iter().collect::<Vec<T>>();
-    sorted.sort();
+    sorted.sort_by(|a, b| a.partial_cmp(b).unwrap_or(Equal));
     let x = sorted.pop().unwrap();
     let y = sorted.pop().unwrap();
     Pair(x, y)
 }
 
+// Fold in Rust nomenclature is what I typically expect to be reduce.
 pub fn fold_max2<I, T>(input: I) -> Pair<T>
 where
     I: IntoIterator<Item = T>,
-    T: Ord,
+    T: Numeric,
 {   
     let mut input = input.into_iter();
     let initial_value = {
@@ -38,12 +55,138 @@ where
     })
 }
 
-fn transform_max2<I, T>(input: I) -> Pair<T>
+fn compare<T: Numeric>(current: PairOption<T>, next: T) -> PairOption<T> {
+    match current {
+        PairOption::Empty => { PairOption::One(next) },
+        PairOption::One(x) => {
+            if next > x {
+                PairOption::Pair(Pair(next, x))
+            } else {
+                PairOption::Pair(Pair(x, next))
+            }
+        },
+        PairOption::Pair(pair) => {
+            if next > pair.0 {
+                PairOption::Pair(Pair(next, pair.0))
+            } else if next > pair.1 {
+                PairOption::Pair(Pair(pair.0, next))
+            } else {
+                current
+            }
+        }
+    }
+}
+
+// Pairs on both sides version of the above function. Bit dicey, and a shame that definitions need to be doubled up
+fn compare_pair<T: Numeric>(current: PairOption<T>, next: PairOption<T>) -> PairOption<T> {
+    match (current, next) {
+        (PairOption::Empty, PairOption::Empty) => PairOption::Empty,
+        (PairOption::Empty, PairOption::One(x)) => PairOption::One(x),
+        (PairOption::One(x), PairOption::Empty) => PairOption::One(x),
+        (PairOption::Empty, PairOption::Pair(pair)) => PairOption::Pair(pair),
+        (PairOption::Pair(pair), PairOption::Empty) => PairOption::Pair(pair),
+        (PairOption::One(x), PairOption::One(y)) => PairOption::Pair( if x > y { Pair(x, y) } else { Pair(y, x) } ),
+        (PairOption::One(x), PairOption::Pair(pair)) => {
+            if x > pair.0 {
+                PairOption::Pair(Pair(x, pair.0))
+            } else if x > pair.1 {
+                PairOption::Pair(Pair(pair.0, x))
+            } else {
+                PairOption::Pair(pair)
+            }
+        },
+        (PairOption::Pair(pair), PairOption::One(x)) => {
+            if x > pair.0 {
+                PairOption::Pair(Pair(x, pair.0))
+            } else if x > pair.1 {
+                PairOption::Pair(Pair(pair.0, x))
+            } else {
+                PairOption::Pair(pair)
+            }
+        },
+        (PairOption::Pair(lhs), PairOption::Pair(rhs)) => {
+            // Hey, look, we're using the sort option here, wasting the last two.
+            // Imagine this is comparable considering the set is limited to 4.
+            let mut nums = vec!(lhs.0, lhs.1, rhs.0, rhs.1);
+            nums.sort_by(|a, b| b.partial_cmp(a).unwrap_or(Equal));
+            PairOption::Pair(Pair(nums[0], nums[1]))
+        },
+    }
+}
+
+// So the previous is pretty slick, but at the end of the day, it's a for loop, right?
+pub fn loop_max2<I, T>(input: I) -> Pair<T>
 where
     I: IntoIterator<Item = T>,
-    T: Ord,
+    T: Numeric
 {
-    unimplemented!()
+    let mut current = PairOption::Empty;
+
+    // Basically the same body in fold.
+    for next in input.into_iter() {
+        match current {
+            PairOption::Empty => { current = PairOption::One(next); },
+            PairOption::One(x) => {
+                if next > x {
+                    current = PairOption::Pair(Pair(next, x));
+                } else {
+                    current = PairOption::Pair(Pair(x, next));
+                }
+            },
+            PairOption::Pair(pair) => {
+                let pair = pair.clone();
+                if next > pair.0 {
+                    current = PairOption::Pair(Pair(next, pair.0));
+                } else if next > pair.1 {
+                    current = PairOption::Pair(Pair(pair.0, next));
+                }
+            }
+        }
+    }
+
+    match current {
+        PairOption::Empty => panic!("Empty input"),
+        PairOption::One(_) => panic!("Insufficient inputs"),
+        PairOption::Pair(pair) => pair
+    }
+
+}
+
+// This is suppsoed to match the article's transform reduce, but that does not really exist in rust.
+// Instead, using the lessons learned from the previous loop function to improve over the fold version.
+pub fn transform_max2<I, T>(input: I) -> Pair<T>
+where
+    I: IntoIterator<Item = T>,
+    T: Numeric,
+{
+    match input.into_iter().fold(PairOption::Empty, compare) {
+        PairOption::Empty => panic!("Empty input"),
+        PairOption::One(_) => panic!("Insufficient inputs"),
+        PairOption::Pair(pair) => pair
+    }
+}
+
+use rayon::prelude::*;
+
+// Now, for the main attraction; parallel!
+pub fn par_max2<I, T>(input: I) -> Pair<T>
+where
+    I: IntoParallelIterator<Item = T>,
+    T: Numeric + Send + Sync,
+{
+    let result = input.into_par_iter()
+        .fold_with(PairOption::Empty, compare)
+        .reduce(|| PairOption::Empty, compare_pair);
+
+    // Aha! This may have been the transform-reduce we've been looking for. The fold_with does a bit of processing, turning into pairs and reducing the set down.
+    // When it's all pairs, we can then do a final reduce with that giga-function we have designed, which must be the equivalent of that function object they were
+    // referring to from C++. However, where they would use function overloading, we can use pattern matching instead.
+
+    match result {
+        PairOption::Empty => panic!("Empty input"),
+        PairOption::One(_) => panic!("Insufficient inputs"),
+        PairOption::Pair(pair) => pair
+    }
 }
 
 #[cfg(test)]
@@ -61,5 +204,20 @@ mod test {
     #[test]
     fn test_fold() {
         assert_eq!(RESULT, fold_max2(SAMPLE));
+    }
+
+    #[test]
+    fn test_loop() {
+        assert_eq!(RESULT, loop_max2(SAMPLE));
+    }
+
+    #[test]
+    fn test_transform() {
+        assert_eq!(RESULT, transform_max2(SAMPLE));
+    }
+
+    #[test]
+    fn test_par() {
+        assert_eq!(RESULT, par_max2(SAMPLE));
     }
 }
